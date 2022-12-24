8d7d70f364534a7fa6db47000d21b25091550867
0KNV1 <ahmadalfaruq098@gmail.com>
2022-12-24 13:31:31 +0700

    test3

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -35,19 +35,19 @@ class HandleInertiaRequests extends Middleware
      * @param  \Illuminate\Http\Request  $request
      * @return array
      */
-    // public function share(Request $request)
-    // {
-    //     $carts_global_count = $request->user() ? Cache::rememberForever('carts_global_count', fn () => Cart::whereBelongsTo($request->user())->whereNull('paid_at')->count()) : null;
-    //     return array_merge(parent::share($request), [
-    //         'auth' => [
-    //             'user' => $request->user(),
-    //         ],
-    //         'ziggy' => fn () => (new Ziggy)->toArray(),
-    //         'categories_global' => Cache::rememberForever('categories_global', fn () => Category::whereHas('products')->get()->map(fn ($q) => [
-    //             'name' => $q->name,
-    //             'slug' => $q->slug,
-    //         ])),
-    //         'carts_global_count' => $carts_global_count,
-    //     ]);
-    // }
+    public function share(Request $request)
+    {
+        $carts_global_count = $request->user() ? Cache::rememberForever('carts_global_count', fn () => Cart::whereBelongsTo($request->user())->whereNull('paid_at')->count()) : null;
+        return array_merge(parent::share($request), [
+            'auth' => [
+                'user' => $request->user(),
+            ],
+            'ziggy' => fn () => (new Ziggy)->toArray(),
+            'categories_global' => Cache::rememberForever('categories_global', fn () => Category::whereHas('products')->get()->map(fn ($q) => [
+                'name' => $q->name,
+                'slug' => $q->slug,
+            ])),
+            'carts_global_count' => $carts_global_count,
+        ]);
+    }
 }
